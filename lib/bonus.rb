8c79511a58592f98208cde24fae540aba181258b
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-11-10 15:34:21 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,9 +20,10 @@ def bonus
    }
   }
 
+  epic_tragedy[:montague][:hero][:status]= "dead"
+  epic_tragedy[:capulet][:heroine][:status]= "dead"
 
-:montague[:hero][:status]= "dead"
-:capulet[:heroine][:status]= "dead"
+
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
